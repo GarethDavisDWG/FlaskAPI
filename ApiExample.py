8ef42168da71de72e383f2d2977614d009925de9
Gareth Davis <gdavis@softwareinstitute.com>
2022-04-26 22:55:11 +0100

    Added loads of comments even though I hate comments because I like you all so much

--- ApiExample.py
+++ ApiExample.py
@@ -1,11 +1,11 @@
-import flask
+import flask #imports flask a framework to help us build a web app
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = flask.Flask(__name__) #creates the flask object to allow access to methods such as app.run
+app.config["DEBUG"] = True  #activates the debugger for when Kieran screws it up otherwise error messages are very cryptic 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])    #tell which HTTP method we are using (GET) and what route (extra bit of the URL) this method will be activated on.  In this case nothing and so home
 def home():
-    return "<h1>Welcome to Gareth's virtual lesson</h1><p>Sorry I am still ill.</p>"
+    return "<h1>Welcome to Gareth's virtual lesson</h1><p>Sorry I am still ill.</p>" #what the api returns
 
-app.run()
+app.run() # method to tell the app to run
